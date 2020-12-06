9cb2f4fb9da4ce343210641aa6d14f3eb49d6207
Jesse Shawl <jesse@jesse.sh>
2020-12-06 07:39:57 -0600

    give up on part 2

--- 2020/04/index.js
+++ 2020/04/index.js
@@ -13,5 +13,36 @@ const input = require("fs")
 
 const validPassport = (p) =>
   ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"].every((key) => key in p);
-
 console.log(input.filter(validPassport).length);
+// Part 2
+const rules = {
+  byr: (d) => 1920 <= Number(d) && Number(d) <= 2002,
+  iyr: (d) => 2010 <= Number(d) && Number(d) <= 2020,
+  eyr: (d) => 2020 <= Number(d) && Number(d) <= 2030,
+  hgt: (d) => {
+    const [, value, unit] = (d || "").match(/([0-9]+)(cm|in)/) || [];
+    if (!value || !unit) return false;
+    if (unit === "cm") {
+      return 150 <= value <= 193;
+    } else {
+      return 59 <= value <= 76;
+    }
+  },
+  hcl: (d) => /#[0-9a-f]{6}/.test(d),
+  ecl: (d) => ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"].includes(d),
+  pid: (d) => /[0-9]{9}/.test(d),
+};
+
+const isValid = (p) => {
+  const out = Object.keys(rules).every((rule) => {
+    const v = p[rule] && rules[rule](p[rule]);
+
+    return v;
+  });
+  if (out) {
+    console.log("Valid", Object.keys(p).length);
+  }
+  return out;
+};
+
+console.log(input.filter(isValid).length);
